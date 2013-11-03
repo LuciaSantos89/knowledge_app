97ef6cd6b6c36f30751107b30825545609a92d51
LuciaSantos89 <analucia0189@gmail.com>
2013-11-03 00:57:07 -0600

    pantalla principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 KnowledgeBowl::Application.routes.draw do
+  
   get ':controller(/:action(/:id(.:format)))'
   post ':controller(/:action(/:id(.:format)))'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
