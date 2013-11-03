e8653999b653199082c00291a038e79906ab522d
LuciaSantos89 <analucia0189@gmail.com>
2013-11-03 03:24:01 -0600

    timer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,23 @@
 $(function(){
     $("#menu").menu({ position: {at: "left" } });
 });
+
+
+function contador(){
+var count=30;
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+  document.getElementById("timer").innerHTML=count + " secs";
+}
+}
